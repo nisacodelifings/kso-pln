de19d7f6ca2a9a697125765360f004a968099662
Glitch (hello-sqlite) <none>
2020-08-12 04:25:32 +0000

    🎙💘 Checkpoint ./public/client.js:8651983/5 ./views/index.html:8651983/89

--- public/client.js
+++ public/client.js
@@ -22,7 +22,7 @@ fetch("/getDreams", {})
 
 // a helper function that creates a list item for a given dream
 const appendNewDream = dream => {
-  const newListItem = document.createElement("li");
+  const newListItem = document.createElement("div");
   newListItem.innerText = dream;
   dreamsList.appendChild(newListItem);
 };
